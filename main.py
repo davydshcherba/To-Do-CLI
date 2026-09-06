92b40672e76131ed055c9bedb5b89aa48bdfc555
davydshcherba <davidserba302@gmail.com>
2026-09-06 13:52:05 +0300

    Add functionality to add new tasks to the to-do list in data.json. Updated main.py to handle "add" command and included a sample task. Refactored data loading for the "list" command to ensure proper file handling.

--- main.py
+++ main.py
@@ -3,8 +3,32 @@ import json
 
 command = input("add, list, done, remove: ")
 
-with open('data.json', 'r', encoding='utf-8') as file:
-    data = json.load(file)
+
 
 if command == "list":
-    print(json.dumps(data, indent=2, ensure_ascii=False))
+    with open('data.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        print(json.dumps(data, indent=2, ensure_ascii=False))
+
+elif command == "add":
+    new_data = {
+        "id": 34,
+        "text": "Lorem ipsum",
+        "done": "False",
+        "created": "09.07.2016"
+    }
+    
+    try:
+        with open("data.json", "r", encoding="utf-8") as file:
+            existing_data = json.load(file)
+    except FileNotFoundError:
+        existing_data = []
+    
+    if not isinstance(existing_data, list):
+        existing_data = [existing_data]
+    existing_data.append(new_data)
+    
+    with open("data.json", "w", encoding="utf-8") as file:
+        json.dump(existing_data, file, indent=2, ensure_ascii=False)
+    
+    print(f"Add! Tasks total: {len(existing_data)}")
