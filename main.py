3e6a1d63b1f85ec275ccb960d83b52e461a92ffb
davydshcherba <davidserba302@gmail.com>
2026-09-07 20:11:18 +0300

    Refactor command input handling in main.py to implement a continuous loop for user commands. Enhanced error handling for invalid inputs by integrating command_checker within the loop, improving user experience and robustness of the CLI.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from utils.command_checker import command_checker
 
-try:
-    command = input("add, list, done, remove: ")
-except ValueError:
-    print("Something went wrong..")
-    print(ValueError)
+while True:
+    try:
+        print("")
+        command = input("add, list, done, remove: ")
+        command_checker(command=command)
+    except ValueError:
+        print("Something went wrong..")
+        print(ValueError)
 
-
-command_checker(command=command)
