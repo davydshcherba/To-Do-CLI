ff2a61fbdaa7dfd7f4f95076ca916cd2c12b53bc
davydshcherba <davidserba302@gmail.com>
2026-09-11 19:07:24 +0300

    Refactor logger decorator to improve date formatting in log entries and remove unnecessary print statements. Update compiled cache file accordingly.

--- utils/decorators/logger.py
+++ utils/decorators/logger.py
@@ -5,14 +5,12 @@ import json
 def logger(func: Callable):
     def wrapper(*args,**kwargs):
         today = datetime.today().strftime('%Y-%m-%d')
-        print(func)
         dataLog = {
             "log": f"{func}",
-            "date": f"{datetime.now()}"
+            "date": f"{datetime.now().strftime("%H:%M:%S")}"
         }
         with open(f"_log/{today}.json", "a", encoding="utf-8") as file:
             json.dump(dataLog, file, indent=2, ensure_ascii=False)
 
-        print(f"Add! Tasks total: {len(dataLog)}")
         func(*args,**kwargs)
     return wrapper
