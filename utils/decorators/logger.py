295877eab1a42dfae7cb6b055c656bef603b4927
davydshcherba <davidserba302@gmail.com>
2026-09-11 18:55:35 +0300

    Enhance logger decorator to log function calls to a JSON file, improving task tracking. Update compiled cache file accordingly.

--- utils/decorators/logger.py
+++ utils/decorators/logger.py
@@ -1,14 +1,15 @@
 from typing import Callable
 from termcolor import colored
+import json
 
 def logger(func: Callable):
     def wrapper(*args,**kwargs):
-        print(colored("|--------------------logger---------------------|", "red"))
-        print("")
-        print(colored(f"{func} was called","red"))
-        print("")
-        print(colored("|--------------------logger---------------------|", "red")) 
-        print(colored("|--------------------result---------------------|", "green"))    
-        print(colored("|--------------------result---------------------|", "green"))       
+        print(func)
+        dataLog = {
+            "log": f"{func}"
+        }
+        with open("_log/11.09.json", "w", encoding="utf-8") as file:
+            json.dump(dataLog, file, indent=2, ensure_ascii=False)
+        print(f"Add! Tasks total: {len(dataLog)}")
         func(*args,**kwargs)
     return wrapper
