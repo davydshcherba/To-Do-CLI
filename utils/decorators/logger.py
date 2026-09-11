2ac129b566095a0b68f25a4eb2205f0b37982c38
davydshcherba <davidserba302@gmail.com>
2026-09-11 19:04:07 +0300

    Update logger decorator to log function calls with date in a new JSON file format. Remove outdated log file and enhance .gitignore to exclude _log directory.

--- utils/decorators/logger.py
+++ utils/decorators/logger.py
@@ -1,15 +1,18 @@
 from typing import Callable
-from termcolor import colored
+from datetime import datetime
 import json
 
 def logger(func: Callable):
     def wrapper(*args,**kwargs):
+        today = datetime.today().strftime('%Y-%m-%d') # 2026-09-11
         print(func)
         dataLog = {
-            "log": f"{func}"
+            "log": f"{func}",
+            "date": f"{datetime.now()}"
         }
-        with open("_log/11.09.json", "w", encoding="utf-8") as file:
+        with open(f"_log/{today}.json", "a", encoding="utf-8") as file:
             json.dump(dataLog, file, indent=2, ensure_ascii=False)
+
         print(f"Add! Tasks total: {len(dataLog)}")
         func(*args,**kwargs)
     return wrapper
