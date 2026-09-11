89f17c15466a3f473cf1051d1a019f5fc6ad43ff
davydshcherba <davidserba302@gmail.com>
2026-09-11 20:09:21 +0300

    Add directory creation for log files in logger decorator to ensure logs are saved correctly. This change enhances the logging functionality by preventing errors when the log directory does not exist.

--- utils/decorators/logger.py
+++ utils/decorators/logger.py
@@ -1,6 +1,7 @@
 from typing import Callable
 from datetime import datetime
 import json
+import os
 
 def logger(func: Callable):
     def wrapper(*args,**kwargs):
@@ -9,6 +10,7 @@ def logger(func: Callable):
             "log": f"{func}",
             "date": f"{datetime.now().strftime("%H:%M:%S")}"
         }
+        os.makedirs("_log", exist_ok=True)
         with open(f"_log/{today}.json", "a", encoding="utf-8") as file:
             json.dump(dataLog, file, indent=2, ensure_ascii=False)
 
